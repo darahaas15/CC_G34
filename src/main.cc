2540af8bf706682205be84bd1a49d1bbf3b8fd6c
Darahaas Yajamanyam <63366288+darahaas15@users.noreply.github.com>
2023-02-14 00:07:27 +0530

    Def undef updated in main without tests

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 #include <cstdio>
 #include <cstring>
 #include <fstream>
@@ -13,6 +14,14 @@ extern FILE *yyin;
 extern int yylex();
 extern char *yytext;
 
+extern FILE *fooin;
+extern FILE *fooout;
+extern int foolex();
+extern char *footext;
+
+extern std::string key;
+extern std::unordered_map<std::string, std::string> map;
+
 NodeStmts *final_values;
 
 #define ARG_OPTION_L 0
@@ -21,99 +30,26 @@ NodeStmts *final_values;
 #define ARG_OPTION_O 3
 #define ARG_FAIL -1
 
-/*void expand_macro(string &str)
-{
-	istringstream ss(str);
-	string line;
-	string expanded_line;
-	while (getline(ss, line))
-	{
-		if (!in_comment)
-		{
-			if (line.find("#def") == 0)
-			{
-				// handle macro definition
-				size_t first_space = line.find(" ");
-				size_t second_space = line.find(" ", first_space + 1);
-				if (second_space == string::npos)
-				{
-					// macro without a body
-					macros[line.substr(first_space + 1)] = "1";
-				}
-				else
-				{
-					string key = line.substr(first_space + 1, second_space - first_space - 1);
-					string value = line.substr(second_space + 1);
-					macros[key] = value;
-				}
-				in_macro = true;
-			}
-			else if (line.find("#undef") == 0)
-			{
-				// handle macro undefinition
-				size_t first_space = line.find(" ");
-				string key = line.substr(first_space + 1);
-				macros.erase(key);
-				in_macro = true;
-			}
-			else
-			{
-				// expand macros in line
-				expanded_line = line;
-				for (auto const &macro : macros)
-				{
-					size_t pos = 0;
-					while ((pos = expanded_line.find(macro.first, pos)) != string::npos)
-					{
-						expanded_line.replace(pos, macro.first.length(), macro.second);
-						pos += macro.second.length();
-					}
-				}
-				// handle comments
-				size_t comment_start = expanded_line.find("//");
-				if (comment_start != string::npos)
-				{
-					expanded_line = expanded_line.substr(0, comment_start);
-				}
-				size_t comment_start_multi = expanded_line.find("");
-				size_t comment_end_multi = expanded_line.find("");
-				if (comment_start_multi != string::npos && comment_end_multi == string::npos)
-				{
-					in_comment = true;
-					expanded_line = expanded_line.sub
-				}
-			}
-		}
-	}
-} */
-
-int parse_arguments(int argc, char *argv[])
-{
-	if (argc == 3 || argc == 4)
-	{
-		if (strlen(argv[2]) == 2 && argv[2][0] == '-')
-		{
-			if (argc == 3)
-			{
-				switch (argv[2][1])
-				{
-				case 'l':
+int parse_arguments(int argc, char *argv[]) {
+	if (argc == 3 || argc == 4) {
+		if (strlen(argv[2]) == 2 && argv[2][0] == '-') {
+			if (argc == 3) {
+				switch (argv[2][1]) {
+					case 'l':
 					return ARG_OPTION_L;
 
-				case 'p':
+					case 'p':
 					return ARG_OPTION_P;
 
-				case 's':
+					case 's':
 					return ARG_OPTION_S;
 				}
-			}
-			else if (argv[2][1] == 'o')
-			{
+			} else if (argv[2][1] == 'o') {
 				return ARG_OPTION_O;
 			}
 		}
-	}
-
+	} 
+	
 	std::cerr << "Usage:\nEach of the following options halts the compilation process at the corresponding stage and prints the intermediate output:\n\n";
 	std::cerr << "\t`./bin/base <file_name> -l`, to tokenize the input and print the token stream to stdout\n";
 	std::cerr << "\t`./bin/base <file_name> -p`, to parse the input and print the abstract syntax tree (AST) to stdout\n";
@@ -122,34 +58,108 @@ int parse_arguments(int argc, char *argv[])
 	return ARG_FAIL;
 }
 
-int main(int argc, char *argv[])
-{
+bool cycle_check(std::unordered_map<std::string, std::string> m) {
+	for(auto i: m) {
+		std::string ptr = i.first;
+		while(m.find(ptr) != m.end()) {
+			ptr = m[ptr];
+			if(ptr == i.first) return true;
+		}
+	}
+	return false;
+}
+
+void preprocess() {
+	// Actual Pre
+	int count;
+	int token;
+	std::string contents;
+	
+	// Run preprocessor until no more macros can be expanded
+	// Preprocessor works on a "temp" file which is removed at the end
+	do {
+		fooin = fopen("temp", "r");
+		count = 0;
+		token = 0;
+		contents = "";
+
+		// Run lexer on program (macro replacing and comment removal)
+		do {
+			token = foolex();
+			std::string temp = footext;
+
+			// Every time a macro is added, check for cycles
+			if(token == 5 && cycle_check(map)) {
+				std::cerr<<"Cycle detected in #def statements"<<std::endl;
+				remove("temp");
+				fclose(fooin);
+				exit(1);
+			}
+
+			// Every time a word is taken in check if it matches macro
+			if(token == 3 && map.find(temp) != map.end()) {
+				count++;
+				temp = map[temp];
+			}
+			contents += temp;
+
+		} while(token != 0);
+
+		std::ofstream otemp("temp");
+		otemp<<contents;
+		otemp.close();
+	} while(count > 0);
+
+	fooin = fopen("temp", "r");
+	contents = "";
+	do {
+		token = foolex();
+		std::string temp = footext;
+		if(token != 1 && token != 2 && token != 5)
+			contents += temp;
+
+	} while(token != 0);
+
+	// Printing final preprocessed code
+	std::cout<<"PRE"<<std::endl<<contents<<std::endl;
+	
+	fclose(fooin);
+
+	std::ofstream ofile("temp");
+	ofile<<contents;
+	ofile.close();
+}
+
+int main(int argc, char *argv[]) {
 	int arg_option = parse_arguments(argc, argv);
-	if (arg_option == ARG_FAIL)
-	{
+	if (arg_option == ARG_FAIL) {
 		exit(1);
 	}
 
+	// Copying main file to temp for preprocessing
 	std::string file_name(argv[1]);
-	FILE *source = fopen(argv[1], "r");
 
-	if (!source)
-	{
-		std::cerr << "File does not exists.\n";
-		exit(1);
+	std::ifstream itemp(file_name);
+	std::ofstream otemp("temp");
+	std::string line;
+	while (getline(itemp, line)) {
+		otemp << line << std::endl;
 	}
+	itemp.close();
+	otemp.close();
 
-	yyin = source;
+	preprocess();
 
-	if (arg_option == ARG_OPTION_L)
-	{
+	// Main Lexer and Parser
+	yyin = fopen("temp", "r");
+
+	// For debugging, prints tokens
+	if (arg_option == ARG_OPTION_L) {
 		extern std::string token_to_string(int token, const char *lexeme);
 
-		while (true)
-		{
+		while (true) {
 			int token = yylex();
-			if (token == 0)
-			{
+			if (token == 0) {
 				break;
 			}
 
@@ -159,35 +169,31 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	final_values = nullptr;
+    final_values = nullptr;
+
+	// Actual lex and parse
 	yyparse();
 
 	fclose(yyin);
+	remove("temp");
 
-	if (final_values)
-	{
-		if (arg_option == ARG_OPTION_P)
-		{
+	if(final_values) {
+		if (arg_option == ARG_OPTION_P) {
 			std::cout << final_values->to_string() << std::endl;
 			return 0;
 		}
-
-		llvm::LLVMContext context;
+		
+        llvm::LLVMContext context;
 		LLVMCompiler compiler(&context, "base");
 		compiler.compile(final_values);
-		if (arg_option == ARG_OPTION_S)
-		{
+        if (arg_option == ARG_OPTION_S) {
 			compiler.dump();
+        } else {
+            compiler.write(std::string(argv[3]));
 		}
-		else
-		{
-			compiler.write(std::string(argv[3]));
-		}
-	}
-	else
-	{
-		std::cerr << "empty program";
+	} else {
+	 	std::cerr << "empty program";
 	}
 
-	return 0;
+    return 0;
 }
