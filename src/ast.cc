aef58ca9c9d1b6b398e94d4342d63fd8a896199a
Darahaas Yajamanyam <63366288+darahaas15@users.noreply.github.com>
2023-04-23 22:22:03 +0530

    Update ast.cc

--- src/ast.cc
+++ src/ast.cc
@@ -3,20 +3,31 @@
 #include <string>
 #include <vector>
 
-NodeBinOp::NodeBinOp(NodeBinOp::Op ope, Node *leftptr, Node *rightptr) {
+NodeBinOp::NodeBinOp(NodeBinOp::Op ope, Node *leftptr, Node *rightptr)
+{
     type = BIN_OP;
     op = ope;
     left = leftptr;
     right = rightptr;
 }
 
-std::string NodeBinOp::to_string() {
+std::string NodeBinOp::to_string()
+{
     std::string out = "(";
-    switch(op) {
-        case PLUS: out += '+'; break;
-        case MINUS: out += '-'; break;
-        case MULT: out += '*'; break;
-        case DIV: out += '/'; break;
+    switch (op)
+    {
+    case PLUS:
+        out += '+';
+        break;
+    case MINUS:
+        out += '-';
+        break;
+    case MULT:
+        out += '*';
+        break;
+    case DIV:
+        out += '/';
+        break;
     }
 
     out += ' ' + left->to_string() + ' ' + right->to_string() + ')';
@@ -24,27 +35,33 @@ std::string NodeBinOp::to_string() {
     return out;
 }
 
-NodeInt::NodeInt(int val) {
+NodeInt::NodeInt(int val)
+{
     type = INT_LIT;
     value = val;
 }
 
-std::string NodeInt::to_string() {
+std::string NodeInt::to_string()
+{
     return std::to_string(value);
 }
 
-NodeStmts::NodeStmts() {
+NodeStmts::NodeStmts()
+{
     type = STMTS;
-    list = std::vector<Node*>();
+    list = std::vector<Node *>();
 }
 
-void NodeStmts::push_back(Node *node) {
+void NodeStmts::push_back(Node *node)
+{
     list.push_back(node);
 }
 
-std::string NodeStmts::to_string() {
+std::string NodeStmts::to_string()
+{
     std::string out = "(begin";
-    for(auto i : list) {
+    for (auto i : list)
+    {
         out += " " + i->to_string();
     }
 
@@ -53,28 +70,36 @@ std::string NodeStmts::to_string() {
     return out;
 }
 
-NodeDecl::NodeDecl(std::string id, Node *expr) {
+// NodeDecl
+
+NodeDecl::NodeDecl(std::string id, Node *expr)
+{
     type = ASSN;
     identifier = id;
     expression = expr;
 }
 
-std::string NodeDecl::to_string() {
+std::string NodeDecl::to_string()
+{
     return "(let " + identifier + " " + expression->to_string() + ")";
 }
 
-NodeDebug::NodeDebug(Node *expr) {
+NodeDebug::NodeDebug(Node *expr)
+{
     type = DBG;
     expression = expr;
 }
 
-std::string NodeDebug::to_string() {
+std::string NodeDebug::to_string()
+{
     return "(dbg " + expression->to_string() + ")";
 }
 
-NodeIdent::NodeIdent(std::string ident) {
+NodeIdent::NodeIdent(std::string ident)
+{
     identifier = ident;
 }
-std::string NodeIdent::to_string() {
+std::string NodeIdent::to_string()
+{
     return identifier;
 }
